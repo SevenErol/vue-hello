0c7b9014b8a5e74744d4838e3021fd1bd3194b15
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-26 14:51:00 +0200

    bonus change color of title with click

--- assets/javascripting/script.js
+++ assets/javascripting/script.js
@@ -6,8 +6,24 @@ createApp({
     return {
 
       message: 'Hello Vue!',
-      url: "https://picsum.photos/200/300"
+      url: "https://picsum.photos/200/300",
+      titleColor: "blue"
 
     }
+  },
+
+  methods: {
+
+    handler () {
+
+        if (this.titleColor === "blue") {
+            this.titleColor = "red"
+            this.message = "ora sono rosso"
+        } else if (this.titleColor === "red"){
+            this.titleColor = "blue"
+            this.message = "ora sono blu"
+        }
+    }
+
   }
 }).mount('#app')
