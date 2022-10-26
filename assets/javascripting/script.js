9dcb62d10201dd88d8288fe3efc86309e23192c1
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-26 14:36:43 +0200

    bonus added image

--- assets/javascripting/script.js
+++ assets/javascripting/script.js
@@ -4,7 +4,10 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+
+      message: 'Hello Vue!',
+      url: "https://picsum.photos/200/300"
+
     }
   }
 }).mount('#app')
